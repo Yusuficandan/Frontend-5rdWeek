bcf48e688103a338f86c63dcf4f9e390f2146b36
Yusuf CANDAN <yusuficandan@gmail.com>
2022-04-21 13:12:46 +0300

    Fomrlar ile append,classlist, create_element metotlari uzerine calismalar yapildi

---
+++ Forms/10-forms.js
@@ -0,0 +1,40 @@
+
+
+(function (window, document, undefined) {
+
+    window.onload = init;
+
+    function init() {
+
+
+        let userFormDOM = document.querySelector('#userForm')
+        userFormDOM.addEventListener('submit', formHandler)
+
+        function formHandler(event) {
+            event.preventDefault()
+            const USER_NAME = document.querySelector("#username")
+            const SCORE = document.querySelector("#score")
+            if (USER_NAME.value && SCORE.value) {
+                addItem(USER_NAME.value, SCORE.value)
+                USER_NAME.value=""
+                SCORE.value=""
+            }
+        }
+ 
+        let userListDOM = document.querySelector('#userList')
+
+        const addItem = (userName, score) => {
+            let liDOM = document.createElement('li')
+            userListDOM.innerHTML = `
+            ${userName}
+            <span class="badge bg-primary rounded-pill">${score}</span>
+            `
+            liDOM.classList.add(
+                'list-group-item', 'd-flex', 'justify-content-between', 'align-items-center'
+            )
+
+            userListDOM.append(liDOM)
+        }
+
+    }
+})(window, document, undefined);
